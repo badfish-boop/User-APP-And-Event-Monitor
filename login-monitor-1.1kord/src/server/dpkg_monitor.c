da1ea99a581f3373241d790dcdd3a6b445e82c3e
hechengwei <hechengwei@kylinos.cn>
2020-10-16 18:01:19 +0800

    用inotify改良

--- login-monitor-1.1kord/src/server/dpkg_monitor.c
+++ login-monitor-1.1kord/src/server/dpkg_monitor.c
@@ -30,25 +30,23 @@ char *event_str[EVENT_NUM] =
  "IN_DELETE_SELF",
  "IN_MOVE_SELF"
 };
-int readFileList(int fd,char *basePath) //初始化监控一级已有子目录
+int read_file_list(int fd,char *basePath) //初始化监控一级已有子目录
 {
      DIR *dir;
      struct dirent *ptr;
      char base[1024];
- 
      if ((dir=opendir(basePath)) == NULL)
      {
          perror("Open dir error...");
          exit(1);
      }
- 
      while ((ptr=readdir(dir)) != NULL)
      {
          if(strcmp(ptr->d_name,".")==0 || strcmp(ptr->d_name,"..")==0)    ///current dir OR parrent dir
              continue;
          else if(ptr->d_type == 4)    ///dir
          {
-			 memset(base,'\0',sizeof(base));
+             memset(base,'\0',sizeof(base));
              strcpy(base,basePath);
              strcat(base,"/");
              strcat(base,ptr->d_name);
@@ -60,117 +58,97 @@ int readFileList(int fd,char *basePath) //初始化监控一级已有子目录
 }
 int inotify_call(char *path)
 {
-
-	 int fd;
-	 int wd;
-	 int len;
-	 int nread;
+	 int fd,wd,len,nread;
 	 char buf[BUFSIZ];
-	 char ch[1024]={0};
-	 memset(ch,0,1024);
+	 char deskinform[1024]={0};
+	 memset(deskinform,0,1024);
 	 struct inotify_event *event;
 	 int i;
-
 	 /*if(argc < 2)
 	 {
 	  fprintf(stderr, "%s path\n", argv[0]);
 	  return -1;
 	 }*/
-
 	 fd = inotify_init();
 	 if( fd < 0 )
 	 {
-		  fprintf(stderr, "inotify_init failed\n");
-		  return -1;
+		 fprintf(stderr, "inotify_init failed\n");
+		 return -1;
 	 }
-
 	 wd = inotify_add_watch(fd, path, IN_CREATE|IN_DELETE);
 	 if(wd < 0)
 	 {
-		  perror(path);
+		 perror(path);
 	 }
-	 readFileList(fd,path);
+	 read_file_list(fd,path);
 	 buf[sizeof(buf) - 1] = 0;
 	 while( (len = read(fd, buf, sizeof(buf) - 1)) > 0 )
 	 {
-		  nread = 0;
-		  while( len > 0 )
-		  {
-			   event = (struct inotify_event *)&buf[nread];
-			   for(i=0; i<EVENT_NUM; i++)
-			   {
-					if((event->mask >> i) & 1)
-					{
-						     
-							 if(event_str[i] == event_str[8])
+		 nread = 0;
+		 while( len > 0 )
+		 {
+			 event = (struct inotify_event *)&buf[nread];
+			 for(i=0; i<EVENT_NUM; i++)
+			 {
+				 if((event->mask >> i) & 1)
+				 { 
+					 if(event_str[i] == event_str[8])
+					 {
+						 char *detect = NULL;
+						 detect = strstr(event->name,".dpkg-new");
+						 if(detect != NULL)
+						 {
+							 char substring[1024] = {0};
+							 char *subdetect = NULL;
+							 memset(substring,0,1024);
+							 strncpy(substring,event->name, detect-event->name);
+							 subdetect = strstr(substring,".");
+							 if(subdetect == NULL)
 							 {
-	
-									char *detect = NULL;
-									detect = strstr(event->name,".dpkg-new");
-									if(detect != NULL)
-									{
-										char substring[1024] = {0};
-										char *subdetect = NULL;
-										memset(substring,0,1024);
-										strncpy(substring,event->name, detect-event->name);
-										subdetect = strstr(substring,".");
-										if(subdetect == NULL)
-										{
-											char subpath[1024] = {0};
-											memset(subpath,'\0',sizeof(subpath));
-											strcpy(subpath,path);
-											strcat(subpath,"/");
-											strcat(subpath, substring);
-											usleep(15);
-											wd = inotify_add_watch(fd, subpath, IN_CREATE|IN_DELETE);
-											if(wd < 0)
-											{
-												  perror(subpath);
-												  return -1;
-											}
-										}
-									}
-							}
-							if(event_str[i] == event_str[8])
-							{
-									char *ret=NULL;
-									ret = strstr(event->name, ".desktop");
-								  	if(ret !=NULL)
-									{
-										char ch1[1024]={0};
-										memset(ch1,0,1024);
-										strncpy(ch1,event->name, ret-event->name);
-										strcpy(ch,ch1);
-										dbus_pkgadd_singal_send(ch);
-									}
-			
-
+								 char subpath[1024] = {0};
+								 memset(subpath,'\0',sizeof(subpath));
+								 strcpy(subpath,path);
+								 strcat(subpath,"/");
+								 strcat(subpath, substring);
+								 usleep(15);
+								 wd = inotify_add_watch(fd, subpath, IN_CREATE|IN_DELETE);
+								 if(wd < 0)
+								 {
+									 perror(subpath);
+									 return -1;
+								 }
 							 }
-							  else if (event_str[i] == event_str[9])
-						      {
-									 char *ret = NULL;
-									 ret = strstr(event->name, ".desktop");
-								  	 if(ret != NULL)
-									 {
-										 char ch1[1024]={0};
-										 memset(ch1,0,1024);
-										 strncpy(ch1,event->name, ret-event->name);
-										 strcpy(ch,ch1);
-										 dbus_pkgremove_singal_send(ch);
-									 }
-							  }
-						      
-					} 
-			   }
-			   nread = nread + sizeof(struct inotify_event) + event->len;
-			   len = len - sizeof(struct inotify_event) - event->len;
-		  }
+						 }
+					 }
+					 if(event_str[i] == event_str[8])
+					 {
+						 char *ret=NULL;
+						 ret = strstr(event->name, ".desktop");
+						 if(ret !=NULL)
+						 {
+							 strncpy(deskinform,event->name, ret-event->name);
+							 dbus_pkgadd_singal_send(deskinform);
+						 }
+					 }
+					 else if (event_str[i] == event_str[9])
+					 {
+						 char *ret = NULL;
+						 ret = strstr(event->name, ".desktop");
+						 if(ret != NULL)
+						 {
+							 strncpy(deskinform,event->name, ret-event->name);
+							 dbus_pkgremove_singal_send(deskinform);
+						 }
+					 }     
+				 } 
+			 }
+		 nread = nread + sizeof(struct inotify_event) + event->len;
+		 len = len - sizeof(struct inotify_event) - event->len;
+		 }
 	 }
 }
-
-
-int CreateDir(const char *sPathName)  
-  {  
+int create_dir(const char *sPathName)  
+{  
       char DirName[256];  
       strcpy(DirName, sPathName);  
       int i,len = strlen(DirName);
@@ -188,19 +166,12 @@ int CreateDir(const char *sPathName)
                   }  
               }  
               DirName[i] = '/';  
-
           }  
       }  
-
       return 0;  
 } 
-
-
 int dpkg_monitor_thread() {
-
     printf("enter dpkg_monitor_thread...\n");
-
     inotify_call("/usr/share/applications");
-    
     return 0;
 }
